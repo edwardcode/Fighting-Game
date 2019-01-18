bdf2dcbbe867f5d79e94fc32adc5afab337fe533
EDWARD YANG <eddiecody888@gmail.com>
2019-01-18 21:29:57 +0000

    v3, all movement delete,left playerand map

--- fighting game/Level1.js
+++ fighting game/Level1.js
@@ -4,9 +4,9 @@ var map;
 var layer;
 
 var player;
-var controls = {};
-var playerSpeed = 150;
-var jumpTimer = 0;
+// var controls = {};
+// var playerSpeed = 150;
+// var jumpTimer = 0;
 
 Game.Level1.prototype = {
   create:function(){
@@ -17,17 +17,10 @@ Game.Level1.prototype = {
     layer = map.createLayer(0);
     layer.resizeWorld();
 
-    map.setCollisionBetween(0,2);
+
 
     player = this.add.sprite(100,560,'player');//position of the player
-    player.anchor.setTo(0.5,0.5);
-    player.body.gravity.y=1000
 
-
-
-    player.animations.add('idle',[0,1],1,true);
-    player.animations.add('jump',[2],1,true);
-    player.animations.add('run',[3,4,5,6,7,8],7,true);
 
 
     this.physics.arcade.enable(player);
@@ -35,36 +28,13 @@ Game.Level1.prototype = {
     player.body.collideWorldBounds = true;
 
 
-    controls = {
-        right: this.input.keboard.addkey(Phaser.Keyboard.d),
-        left:this.input.keboard.addkey(Phaser.Keyboard.a),
-        up:this.input.keboard.addkey(Phaser.Keyboard.w),
-
-
-    };
 
   },
 
   update:function(){
-     this.physics.arcade.collide(player,layer);
-
-      if(controls.up.isDown){
-          player.animations.play('jump');
-      }
-
-      if(controls.right.isDown){
-        player.animations.play('run');
-        player.scale.setTo(1,1);
-        player.body.velocity.x += playerSpeed;
-      }
-
-       if(controls.left.isDown){
-        player.animations.play('idle');
-        player.scale.setTo(-1,1);
-        player.body.velocity.x -= playerSpeed;
-      }
+     this.physics.arcade.collide(player,layer)
 
 
-
+  },
 
 }
